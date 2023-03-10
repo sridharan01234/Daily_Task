c5e6bf5f63d53130af463d1c1b85445255b7b8f1
sridharan01234 <sridharan01234@gmail.com>
2023-03-10 14:12:58 +0530

    _SERVER

---
+++ 10.03.2023/golbalVariables/_SERVER.php
@@ -0,0 +1,6 @@
+<?php
+echo $_SERVER['SERVER_NAME']."<br>"; //localhost
+echo $_SERVER['PHP_SELF']."<br>"; ///10.03.2023/golbalVariables/_SERVER.php
+echo $_SERVER['SERVER_SOFTWARE']."<br>"; //PHP 7.4.3-4ubuntu2.18 Development Server
+echo $_SERVER['SERVER_PROTOCOL']."<br>"; // HTTP/1.1
+echo $_SERVER['REQUEST_METHOD']."<br>"; //GET
