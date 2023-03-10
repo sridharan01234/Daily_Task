a29d76b970adaac55698636a9c45824c8b6695b7
sridharan01234 <sridharan01234@gmail.com>
2023-03-10 09:34:00 +0530

    namespace

---
+++ 09.03.2023/Namespace.php
@@ -0,0 +1,13 @@
+<?php
+require 'NamespaceUtils.php';
+use MyApp\Utils\Logger;
+use function MyApp\Utils\format_string;
+
+// use the Logger class
+$logger = new Logger();
+$logger->log('Hello, world!');
+
+// use the format_string function
+$str = '  hello, world!  ';
+echo format_string($str);
+?>
---
+++ 09.03.2023/NamespaceUtil.php
@@ -0,0 +1,13 @@
+<?php
+namespace MyApp\Utils;
+
+function format_string($str) {
+    return ucfirst(trim($str));
+}
+
+class Logger {
+    public function log($message) {
+        // code to log message
+    }
+}
+?>
