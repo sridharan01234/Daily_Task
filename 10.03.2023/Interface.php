24455293b56a6a396c66bdc8a73328f108b0319f
sridharan01234 <sridharan01234@gmail.com>
2023-03-10 14:12:20 +0530

    Interfaces

---
+++ 10.03.2023/Interface.php
@@ -0,0 +1,23 @@
+<?php
+interface Animal {
+  public function makeSound();
+}
+
+class Cat implements Animal {
+  public function makeSound() {
+    echo "Meow <br>";
+  }
+}
+
+  class Dog implements Animal {
+    public function makeSound() {
+      echo "Bow Bow <br>";
+    }
+}
+
+$cat = new Cat();
+$cat->makeSound();
+
+$dog = new Dog();
+$dog->makeSound();
+?> 
